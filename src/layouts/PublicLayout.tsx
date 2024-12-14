87334bbe766da8fce3d6b2bc3c44cf04f4c1470c
Jose Pedro Silva <josepedro88@live.com.pt>
2024-12-14 14:34:21 +0100

    Implement typewriter effect in PublicLayout with updated phrases and interval

--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -1,21 +1,21 @@
 import { Outlet } from 'react-router-dom';
 import './PublicLayout.css'
 
-// import useTypewriter from '../hooks/useTypewriter';
+import useTypewriter from '../hooks/useTypewriter';
 
 const PublicLayout = () => {
-  // const PHRASES_ROTATION = [
-  //   'down your ideas before they fade away.',
-  //   'your notes, your life.',
-  //   'your thoughts, your notes.',
-  //   'your notes, your world.',
-  //   'your notes, your way.',
-  //   'your notes, your style.',
-  // ];
+  const PHRASES_ROTATION = [
+    'down your ideas before they fade away.',
+    'your notes, your life.',
+    'your thoughts, your notes.',
+    'your notes, your world.',
+    'your notes, your way.',
+    'your notes, your style.',
+  ];
 
-  // const INTERVAL = 150;
+  const INTERVAL = 75;
 
-  // const text = useTypewriter(PHRASES_ROTATION, INTERVAL);
+  const text = useTypewriter(PHRASES_ROTATION, INTERVAL);
 
   return (
     <div className='public-layout-container'>
@@ -24,7 +24,7 @@ const PublicLayout = () => {
       </div>
       <div className="public-layout-right">
         <h1>
-          <span className='slogan-accent-span'>Note {}</span><span className='cursor'>|</span>
+          <span className='slogan-accent-span'>Note {text}</span><span className='cursor'>|</span>
         </h1>
       </div>
     </div>
