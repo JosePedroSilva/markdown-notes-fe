56d52ef03a2c3bfc09d1981600bc8577c83e36d0
Jose Pedro Silva <josepedro88@live.com.pt>
2024-12-18 19:51:00 +0100

    Add PrivateLayout component with sidebar and update global background color

--- src/layouts/PrivateLayout.tsx
+++ src/layouts/PrivateLayout.tsx
@@ -1,10 +1,17 @@
 import { Outlet } from 'react-router-dom';
+import './PrivateLayout.css'
+
+import SideBar from '../components/SideBar';
 
 const PrivateLayout = () => {
   return (
-    <div>
-      Private layout
-      <Outlet />
+    <div className="private-layout">
+      <div className="sidebar-layout">
+        <SideBar />
+      </div>
+      <div className="content-layout">
+        <Outlet />
+      </div>
     </div>
   );
 };
